af8faaa3b4a3c52da5f301519fbdc68a35a0a25d
Mphumudzeni <mphumu.tshiswaise@GMAIL.COM>
2024-08-29 15:30:25 +0200

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+  let input = document.getElementById("petInput").value;
+  // Validation logic goes here
+  let result = ""; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+  // TODO: Write your validation logic here
+  // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-            document.getElementById('result').innerText = result;
+  const validSyntax = /^pet_[0-9]{4}[a-zA-Z0-9]+$/;
+
+  if (validSyntax.test(input)) {
+    result = 'Valid Syntax <span class="valid"></span>';
+  } else {
+    result = 'Invalid Syntax <span class="invalid"></span>';
+  }
+
+  document.getElementById("result").innerHTML = result;
 }
-
 
